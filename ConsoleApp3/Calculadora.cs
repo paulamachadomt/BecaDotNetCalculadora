40a9e8374f898ea9cc87f97c079265259a5657f4
paulamachadomt <paulamachadomt@gmail.com>
2020-12-14 21:48:02 -0300

    atividade calculadora beca .net

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,20 +1,98 @@
-﻿namespace Projeto
+﻿using System;
+using System.Globalization;
+
+namespace Projeto
 {
     public class Calculadora
     {
-        // Soma(double n1, double n2)
-        public double Soma(double n1, double n2)
+        private double Valor1 { get; set; }
+        private double Valor2 { get; set; }
+        private string Operador { get; set; }
+        private double Resultado { get; set; }
+        private string DadosDigitadosOperacao { get; set; }
+        public bool Error { get; set; }
+
+
+        public Calculadora(double valor1, double valor2, string operador, double resultado, string dadosDigitadosOperacao, bool error)
         {
-            return n1 + n2;
+            Valor1 = valor1;
+            Valor2 = valor2;
+            Operador = operador;
+            Resultado = resultado;
+            DadosDigitadosOperacao = dadosDigitadosOperacao;
+            Error = false;
         }
-        public double Soma(params double[] numeros)
+
+        public Calculadora()
         {
-            double resultado = 0.0;
-            foreach (double numero in numeros)
+        }
+
+        public void LeituraDadosOperacao()
+        {
+            Console.WriteLine("=====================Calculadora C#=====================");
+            Console.WriteLine();
+            System.Console.WriteLine("Digite a operação que deseja calcular (Ex: 20+45)  \nOperadores disponíveis: +   -  /   *  \nAperte Enter para calcular");
+            Console.WriteLine();
+            DadosDigitadosOperacao = Console.ReadLine();
+        }
+
+        private void IdentificaOperador()
+        {
+            if (DadosDigitadosOperacao.Contains("+"))
             {
-                resultado += numero;
+                Operador = "+";
             }
-            return resultado;
+            if (DadosDigitadosOperacao.Contains("-"))
+            {
+                Operador = "-";
+            }
+            if (DadosDigitadosOperacao.Contains("/"))
+            {
+                Operador = "/";
+            }
+            if (DadosDigitadosOperacao.Contains("*"))
+            {
+                Operador = "*";
+            }
+        }
+
+        public void TrataDadosOperacao()
+        {
+            IdentificaOperador();
+            string[] caracteresOperacao = DadosDigitadosOperacao.Split(Operador[0]);
+            Valor1 = double.Parse(caracteresOperacao[0], CultureInfo.InvariantCulture);
+            Valor2 = double.Parse(caracteresOperacao[1], CultureInfo.InvariantCulture);
+            if (Valor2 == 0)
+            {
+                Error = true;
+                Console.WriteLine("Impossível divisão por zero!");
+            }
+        }
+
+        public void Calcular()
+        {
+            if (!Error)
+            {
+                switch (Operador)
+                {
+                    case "+":
+                        Resultado = Valor1 + Valor2;
+                        break;
+                    case "-":
+                        Resultado = Valor1 - Valor2;
+                        break;
+                    case "/":
+                        Resultado = Valor1 / Valor2;
+                        break;
+                    case "*":
+                        Resultado = Valor1 * Valor2;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("Resultado: " + Resultado.ToString("F2"));
+                Console.ReadLine();
+            }
         }
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Projeto
-{  
+{
     class Program
-    { 
+    {
         public static void Main(String[] args)
         {
-            Calculadora calc;
-            calc = new Calculadora();
 
-            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
+            string continuar = "s";
 
-            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (continuar.Equals("s"))
+            {
+                try
+                {
+                    Calculadora calculadora = new Calculadora();
 
-            double resultado = calc.Soma(n1, n2);
-            Console.WriteLine("resultado 1 =" + resultado);
+                    calculadora.LeituraDadosOperacao();
 
+                    calculadora.TrataDadosOperacao();
 
-            double resultado2 = calc.Soma(numeros);
-            Console.WriteLine("resultado 2 =" + resultado2);
+                    calculadora.Calcular();
 
-
-            Console.Read();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valores inválidos. \n");                    
+                }
+                Console.Write("Deseja continuar? (S/N) ");
+                continuar = Console.ReadLine().ToLower();
+                Console.Clear();
+            }                                    
         }
     }
-
 }
